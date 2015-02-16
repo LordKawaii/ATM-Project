8315e859b0583ed4bc711d4b1143e3299bc4c640
Jody <crowsonjj@gmail.com>
2015-02-16 15:58:44 -0800

    Added User class.  Bunch of errors, and not sure how to implement NUnit.framework.

--- atm-project/atm-project/atm-project.cpp
+++ atm-project/atm-project/atm-project.cpp
@@ -1,11 +1,29 @@
 // atm-project.cpp : Defines the entry point for the console application.
 //
-#include "NUnit.framework"
+
 #include "stdafx.h"
+#include <cstring>
+#include <string>
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+	[Testfixture]
+	
+	[Test]
+	Assert.AreEqual(true, User.setName("JJ"));
+	Assert.AreEqual(false, User.setName(""));
+
+	[Test]
+	Assert.AreEqual(true, User.zipCode(97478));
+	Assert.AreEqual(false, User.zipCode(123));
+
+	[Test]
+	Assert.AreEqual(true, User.setDob("Feb 8, 1989"));
+	Assert.AreEqual(false, User.setDob(""));
+
+	[Test]
+	Assert.AreEqual(true, User.addCard(Card));
+	Assert.AreEqual(false, User.addCard());
 }
 
