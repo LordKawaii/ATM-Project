c4e539301dc3980e230ef5572db72a26e1928fe1
Friction-less-development <q1p0w@yahoo.com>
2015-02-17 13:33:44 -0800

    ojsfixed errors in card

--- atm-project/test project/cardTest.h
+++ atm-project/test project/cardTest.h
@@ -1,21 +1,22 @@
 #pragma once
-ref class cardTest
+
 #include "card.h"
 
 using namespace NUnit::Framework;
+ref class cardTest
 {
 public:
 	cardTest();
 	[Test]
 	//Is INPUT 4 digits
-	void isConPINCorrect()
+	void inputCorrectLength()
 	{
 		card testCard(10, 1234, 20161209);
-		Assert.AreEqual(false, testCard.grantAccess(1234));
-		card testCard(113456540546, 12, 20161209);
-		Assert.AreEqual(false, testCard.grantAccess(12));
-		card testCard(113456540546, 1234, 201209);
-		Assert.AreEqual(false, testCard.grantAccess(1234));
+		Assert::AreEqual(false, testCard.grantAccess(1234));
+		card testCard2(113456540546, 12, 20161209);
+		Assert::AreEqual(false, testCard2.grantAccess(12));
+		card testCard3(113456540546, 1234, 201209);
+		Assert::AreEqual(false, testCard3.grantAccess(1234));
 	}
 
 	[Test]
@@ -23,8 +24,8 @@ public:
 	void isConPINCorrect()
 	{
 		card testCard(102323498504, 1234, 20161209);
-		Assert.AreEqual(true, testCard.grantAccess(1234));
-		Assert.AreEqual(false, testCard.grantAccess(2134));
+		Assert::AreEqual(true, testCard.grantAccess(1234));
+		Assert::AreEqual(false, testCard.grantAccess(2134));
 	}
 
 	[Test]
@@ -33,47 +34,47 @@ public:
 	void isPINchanged()
 	{
 		card testCard(102323498504, 1234, 20161209);
-		Assert.AreEqual(true, testCard.changePIN(1234, 6666));
-		Assert.AreEqual(false, testCard.changePIN(2134, 5555));
+		Assert::AreEqual(true, testCard.changePIN(1234, 6666));
+		Assert::AreEqual(false, testCard.changePIN(2134, 5555));
 	}
 
 	[Test]
 	//Is conformation PIN isn't the correct PIN 3 times is the system locked?
-	void isPINchanged()
+	void isLockoutWorking()
 	{
 		card testCard(102323498504, 1234, 20161209);
-		Assert.AreEqual(false, testCard.grantAccess(5643, 6666));
-		Assert.AreEqual(false, testCard.grantAccess(2134, 5555));
-		Assert.AreEqual(false, testCard.grantAccess(2134, 5555));
-		Assert.AreEqual(true, testCard.grantAccess(1234, 5555));
+		Assert::AreEqual(false, testCard.changePIN(5643, 6666));
+		Assert::AreEqual(false, testCard.changePIN(2134, 5555));
+		Assert::AreEqual(false, testCard.changePIN(2134, 5555));
+		Assert::AreEqual(true, testCard.changePIN(1234, 5555));
 	}
 
 	//checking if card is valid
-	[Test]
+/*	[Test]
 	//Is the user-entered PIN the correct PIN stored on the card?
 	void isPINchanged()
 	{
 		card testCard(102323498504, 1234, 20161209);
-		Assert.AreEqual(true, testCard.grantAccess(1234));
-		Assert.AreEqual(false, testCard.grantAccess(2345));
-	}
+		Assert::AreEqual(true, testCard.grantAccess(1234));
+		Assert::AreEqual(false, testCard.grantAccess(2345));
+	}*/
 
 	[Test]
 	//Is the card before it's expiration date?
-	void isPINchanged()
+	void isExpired()
 	{
 		card testCard(102323498504, 1234, 20161209);
-		Assert.AreEqual(false, testCard.expired(20150214));
-		Assert.AreEqual(false, testCard.expired(20181212));
+		Assert::AreEqual(false, testCard.expired(20150214));
+		Assert::AreEqual(false, testCard.expired(20181212));
 	}
 
 	[Test]
 	//Is the card number correct?
-	void isNumCorrect
+	void isNumCorrect()
 	{
 		card testCard(102323498504, 1234, 20161209);
-		Assert.AreEqual(true, testCard.checkCardNum(102323498504));
-		Assert.AreEqual(false, testCard.checkCardNum(201812120275));
+		Assert::AreEqual(true, testCard.checkCardNum(102323498504));
+		Assert::AreEqual(false, testCard.checkCardNum(201812120275));
 
 	}
 };
