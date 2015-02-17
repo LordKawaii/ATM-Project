8825fa854f8461439646c2049eb1f69cbde03735
Jody <crowsonjj@gmail.com>
2015-02-17 13:14:19 -0800

    update

--- atm-project/test project/cardTest.h
+++ atm-project/test project/cardTest.h
@@ -1,6 +1,7 @@
 #pragma once
-ref class cardTest
 #include "card.h"
+using namespace NUnit::Framework;
+ref class cardTest
 {
 public:
 	cardTest();
@@ -8,16 +9,16 @@ public:
 	//Is INPUT 4 digits
 	void isConPINCorrect()
 	{
-		Assert.AreEqual(false, testCard.grantAccess(1234213));
-		Assert.AreEqual(true, testCard.grantAccess(1234));
+		Assert::AreEqual(false, testCard.grantAccess(1234213));
+		Assert::AreEqual(true, testCard.grantAccess(1234));
 	}
 
 	[Test]
 	//Is confirmation PIN the correct PIN?
 	void isConPINCorrect()
 	{
-		Assert.AreEqual(true, testCard.grantAccess(1234));
-		Assert.AreEqual(false, testCard.grantAccess(2134));
+		Assert::AreEqual(true, testCard.grantAccess(1234));
+		Assert::AreEqual(false, testCard.grantAccess(2134));
 	}
 
 	[Test]
@@ -25,18 +26,18 @@ public:
 	//Is conformation PIN isn't the correct PIN is the PIN left unchanged?
 	void isPINchanged()
 	{
-		Assert.AreEqual(true, testCard.changePIN(1234, 6666));
-		Assert.AreEqual(false, testCard.changePIN(2134, 5555));
+		Assert::AreEqual(true, testCard.changePIN(1234, 6666));
+		Assert::AreEqual(false, testCard.changePIN(2134, 5555));
 	}
 
 	[Test]
 	//Is conformation PIN isn't the correct PIN 3 times is the system locked?
 	void isPINchanged()
 	{
-		Assert.AreEqual(false, testCard.grantAccess(5643, 6666));
-		Assert.AreEqual(false, testCard.grantAccess(2134, 5555));
-		Assert.AreEqual(false, testCard.grantAccess(2134, 5555));
-		Assert.AreEqual(true, testCard.grantAccess(1234, 5555));
+		Assert::AreEqual(false, testCard.grantAccess(5643, 6666));
+		Assert::AreEqual(false, testCard.grantAccess(2134, 5555));
+		Assert::AreEqual(false, testCard.grantAccess(2134, 5555));
+		Assert::AreEqual(true, testCard.grantAccess(1234, 5555));
 	}
 
 	//checking if card is valid
@@ -44,16 +45,16 @@ public:
 	//Is the user-entered PIN the correct PIN stored on the card?
 	void isPINchanged()
 	{
-		Assert.AreEqual(true, testCard.grantAccess(1234));
-		Assert.AreEqual(false, testCard.grantAccess(2345));
+		Assert::AreEqual(true, testCard.grantAccess(1234));
+		Assert::AreEqual(false, testCard.grantAccess(2345));
 	}
 
 	[Test]
 	//Is the card before it's expiration date?
 	void isPINchanged()
 	{
-		Assert.AreEqual(false, testCard.expired(20150214));
-		Assert.AreEqual(false, testCard.expired(20181212));
+		Assert::AreEqual(false, testCard.expired(20150214));
+		Assert::AreEqual(false, testCard.expired(20181212));
 	}
 };
 
