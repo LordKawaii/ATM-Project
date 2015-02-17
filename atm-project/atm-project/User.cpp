c5122532f9caad0aa348fe122845dc5b808f205b
Jody <crowsonjj@gmail.com>
2015-02-16 16:00:09 -0800

    did my stuff

---
+++ atm-project/atm-project/User.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include "User.h"
+#include <string>
+#include <cstring>
+
+User::User()
+{
+}
+
+
+User::~User()
+{
+}
+
+
+User::setName(std::string name)
+{
+	if (name != "")
+	{
+		return true;
+	}
+	return false;
+}
+
+User::setZipcode(int zip)
+{
+	if (zip.rbegin - zip.rend = 5)
+	{
+		return true;
+	}
+	return false;
+}
+
+User::setDob(std::string DOB)
+{
+	if (DOB != "")
+	{
+		return true;
+	}
+	return false;
+}
+
+User::addCard(Card card)
+{
+	for (int i = 0; i < Card; i++)
+	{
+		if (card == Card[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
