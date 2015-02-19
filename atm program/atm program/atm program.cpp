25e3f0f6c03e6d84c21caed9447bf050650e9d9b
Jody <crowsonjj@gmail.com>
2015-02-19 13:05:55 -0800

    Revert "Revert "created new project""
    This reverts commit 625abe2409b069b6dc48d12cb75aded196ebd75d.

---
+++ atm program/atm program/atm program.cpp
@@ -0,0 +1,18 @@
+// atm program.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
