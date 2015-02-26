dcd4ac5c18f4df0fef431771144c842c78e61cde
Friction-less-development <q1p0w@yahoo.com>
2015-02-26 12:47:50 -0800

    uhuhuhuh duuuuhh duall uuuulrurururur  Please enter the commit message for your changes. Lines starting

--- atm program/atm program/atm program.cpp
+++ atm program/atm program/atm program.cpp
@@ -15,8 +15,11 @@ using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
 	std::string name, DOB;
-	int zipCode, Card;
+	int zipCode, cardNumberInput;
+	int todaysDate = 20150226;
+
 	User tempName, tempZip, tempDOB, tempCard;
+	card * theOneCard = card(1223334444555556, 1234, 20181212);
 
 	std::cout << "Please enter your name: ";
 	std::cin >> name;
@@ -34,9 +37,22 @@ int _tmain(int argc, _TCHAR* argv[])
 	tempDOB.setDob(DOB);
 
 	std::cout << "Please enter your card number: ";
-	std::cin >> Card;
+	std::cin >> cardNumberInput;
 
-	tempCard.addCard(Card);
+	tempCard.addCard(cardNumberInput);
+
+	if (theOneCard.cardNumberScale(cardNumberInput) == true)
+	{
+		if (theOneCard.expired(todaysDate))
+		{
+			std::cout << "You're card is expired idiot" << endl;
+			//do something to get rid of this idiot.
+		}
+		if (theOneCard.lockedOut() == true)
+		std::cout << "Please enter your PIN" << endl;
+
+	}
+
 
 	return 0;
 }
